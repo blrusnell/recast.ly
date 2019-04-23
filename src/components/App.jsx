c62bf0f743f2cf00b27a3abfdea8b71dd5a6fd9a
blrusnell <blrusnell@gmail.com>
2019-04-23 17:26:58 -0500

    live search working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,13 @@ import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 import searchYouTube from '../lib/searchYouTube.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
+import Search from './Search.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.initializeVideos = this.initializeVideos.bind(this);
+    this.onSearchListChange = this.onSearchListChange.bind(this);
     
     this.options = {
       q: 'dogs',
@@ -17,14 +19,13 @@ class App extends React.Component {
       videoEmbeddable: true,
       part: 'snippet',
       type: 'video',
-
-    }
+    };
     
 
     this.state = {
       videos: exampleVideoData,
       currentVideo: exampleVideoData[0]
-    }
+    };
   }
 
   initializeVideos(data) {
@@ -41,31 +42,35 @@ class App extends React.Component {
     });
   }
 
+  onSearchListChange(event) {
+    this.options.q = event.target.value;
+    searchYouTube(this.options, this.initializeVideos);
+  }
+
   
 
   render() {
     let newVidProps = {
       click: this.onVideoListClick.bind(this),
       videos: this.state.videos
-    }
+    };
 
     return (
       <div>
-      <nav className="navbar">
-        <div className="col-md-6 offset-md-3">
-          <div><h5><em>search</em> view goes here</h5></div>
-        </div>
-      </nav>
-      <div className="row">
-        <div className="col-md-7">
-          <VideoPlayer video={this.state.currentVideo} />
-        </div>
-
-        <div className="col-md-5" >
-          <VideoList {...newVidProps} />
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search change={this.onSearchListChange}/>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentVideo} />
+          </div>
+          <div className="col-md-5" >
+            <VideoList {...newVidProps} />
+          </div>
         </div>
       </div>
-    </div>
     );
   }
   componentDidMount() {
